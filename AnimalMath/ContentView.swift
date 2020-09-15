066881aa8f0155c1fd3d2221e065a4e99585a1d7
Markus Seidler <mseidler79@gmail.com>
2020-09-15 20:46:35 +0800

    Initial Commit

---
+++ AnimalMath/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimalMath
+//
+//  Created by Markus Seidler on 15/9/20.
+//  Copyright © 2020 Mata. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
