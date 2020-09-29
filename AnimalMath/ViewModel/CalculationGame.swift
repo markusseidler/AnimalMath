796cf94ade31141dfff76e0bae102b0e4f3dedca
Markus Seidler <mseidler79@gmail.com>
2020-09-29 22:27:22 +0800

    working on implement input and output arrays in VM for view

--- AnimalMath/ViewModel/CalculationGame.swift
+++ AnimalMath/ViewModel/CalculationGame.swift
@@ -22,6 +22,8 @@ class CalculationGame: ObservableObject {
     var animalSelected: DisplayItem<String>? { animalModel.animalSelected}
     
     var calculationType: CalculationTypes
+    var calculationInput = [String : [DisplayItem<String>]]()
+    var calculationOutput: Double? 
     
     init() {
         self.colorModel = ColorModel()
@@ -36,8 +38,11 @@ class CalculationGame: ObservableObject {
     @Published private var calculationGame: CalculationTable<String>?
     @Published private var colorModel: ColorModel
     @Published private var animalModel: AnimalModel
-
     
+    private var leftAnimalInputArray = [[DisplayItem<String>]]()
+    private var rightAnimalInputArray = [[DisplayItem<String>]]()
+    private var resultArray = [Double]()
+
 
     // MARK: - Public methods, User Intent
     
@@ -49,13 +54,74 @@ class CalculationGame: ObservableObject {
         animalModel.animalTapped(animal: animalName)
     }
     
-    
     func startGame() {
         if animalSelected == nil {
             selectAnimal(animalName: "panda")
         }
         calculationGame = CalculationTable(numberOfQuestionsEnum: maxNumber, highestNumber: highestNumber, displayContentArray: animalArray, operation: calculationType.asFunction)
     }
+    
+    // MARK: - Private methods
+    
+    private func createAnimalInput() {
+        // is this needed? do I need to clean them before use them again?
+//        leftAnimalInputArray = [[DisplayItem<String>]]()
+        //        rightAnimalInputArray = [[DisplayItem<String>]]()
+        //        resultArray = [Double]()
+        
+        guard let results = calculationGame?.calculationResults else { return }
+        guard let leftSideNumbers = calculationGame?.leftSideNumbers else { return }
+        guard let rightSideNumbers = calculationGame?.rightSideNumbers else { return }
+        
+        resultArray = results
+        for leftNumber in leftSideNumbers {
+            
+        }
+        
+        
+    }
+    
+    // TODO: implement this... and then work on gameview...
+    
+    
+//    func getInputAndOutput() {
+   //        calculationInput["left"] = leftAnimalInputArray.remove(at: 0)
+   //        calculationInput["right"] = rightAnimalInputArray.remove(at: 0)
+   //        calculationOutput = resultArray.remove(at: 0)
+   ////        print(calculationInput)
+   //    }
+   //
+   //
+   //    func createAnimalInput() {
+   //        leftAnimalInputArray = [[DisplayItem<String>]]()
+   //        rightAnimalInputArray = [[DisplayItem<String>]]()
+   //        resultArray = [Double]()
+   //
+   //        resultArray = multiplicationTable.calculationResults
+   //
+   //        for leftNumber in multiplicationTable.leftSideNumbers {
+   //            var tempLeftArray = [DisplayItem<String>]()
+   //
+   //            for _ in 0..<Int(leftNumber) {
+   //                tempLeftArray.append(DisplayItem<String>(content: animalNameTapped))
+   ////                tempLeftArray.append(animalSelected!)
+   //            }
+   //            leftAnimalInputArray.append(contentsOf: [tempLeftArray])
+   //        }
+   //
+   //        for rightNumber in multiplicationTable.rightSideNumbers {
+   //            var tempRightArray = [DisplayItem<String>]()
+   //
+   //            for _ in 0..<Int(rightNumber) {
+   //                tempRightArray.append(DisplayItem<String>(content: animalNameTapped))
+   ////                tempRightArray.append(animalSelected!)
+   //            }
+   //            rightAnimalInputArray.append(contentsOf: [tempRightArray])
+   //        }
+   //    }
+   //
+    
+    
     
 }
 
